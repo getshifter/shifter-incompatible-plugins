32105624c42ac8ed85c57d67ce44d677aa55c13a
wokamoto <wokamoto@digitalcube.jp>
2019-06-25 14:11:11 +0900

    Modified messages (unrecommended -> incompatible)

--- create-json.php
+++ create-json.php
@@ -1,14 +1,17 @@
 <?php
-$unrecommendedPlugins = [];
-$url = 'https://support.getshifter.io/articles/2833720-unrecommended-wordpress-plugins';
+$url = 'https://support.getshifter.io/articles/3086606-incompatible-wordpress-plugins';
+$incompatiblePlugins = [
+    'supportUrl' => $url,
+    'plugins' => [],
+];
 $dom = new DOMDocument;
 @$dom->loadHTMLFile($url);
 $xpath = new DOMXPath($dom);
 $xpathQuery = '//article[@dir="ltr"]/ul[2]/li';
 foreach ($xpath->query($xpathQuery) as $node) {
-    $unrecommendedPlugin = strtolower(str_replace(' ', '-', $node->nodeValue));
-    if (! in_array($unrecommendedPlugin, $unrecommendedPlugins)) {
-        $unrecommendedPlugins[] = $unrecommendedPlugin;
+    $incompatiblePlugin = strtolower(str_replace(' ', '-', $node->nodeValue));
+    if (! in_array($incompatiblePlugin, $incompatiblePlugins)) {
+        $incompatiblePlugins['plugins'][] = $incompatiblePlugin;
     }
 }
-echo json_encode($unrecommendedPlugins);
+echo json_encode($incompatiblePlugins);
